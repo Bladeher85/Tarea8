abb432779d50dbef7c8f3fec5b08a06df95147c6
Bladeher85 <89565946+Bladeher85@users.noreply.github.com>
2021-09-09 08:58:38 -0600

    Update Alumno.java

--- tarea8/Alumno.java
+++ tarea8/Alumno.java
@@ -9,6 +9,8 @@ package tarea8;
  *
  * @author Phernandez
  */
+ //Pedro Luis de Jesus Hernandez Lopez
+ //Carnet: 7690-20-16633
 public class Alumno implements Comparador {
 
     public String carnet;
